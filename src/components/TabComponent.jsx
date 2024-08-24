8407aa197d00b01a88571a93b9465a148f18e331
Sara Mohammadi <sara.mhmdii79@gmail.comgit config --global user.email sara.mhmdii79@gmail.com>
2024-08-25 00:07:49 +0330

    Add localStorage support for saving selected tab

--- src/components/TabComponent.jsx
+++ src/components/TabComponent.jsx
@@ -4,14 +4,24 @@ import TabContent from "./TabContent";
 import { tabs } from "../constants/constants";
 import "../styles/TabComponent.css";
 function TabComponent() {
-  const [selectedTab, setSelectedTab] = useState(tabs[0].id);
+  const [selectedTab, setSelectedTab] = useState(() => {
+    const savedTab = localStorage.getItem("selectedTab");
+    return savedTab ? parseInt(savedTab, 10) : tabs[0].id;
+  });
   const activeTabData = tabs.find((tab) => tab.id === selectedTab);
-
+  const clickHandler = (id) => {
+    setSelectedTab(id);
+    localStorage.setItem("selectedTab", id);
+  };
   return (
     <>
       <div id="buttons">
         {tabs.map((tab) => (
-          <button key={tab.id} onClick={() => setSelectedTab(tab.id)}  className={tab.id === selectedTab ? "active" : ""}>
+          <button
+            key={tab.id}
+            onClick={() => clickHandler(tab.id)}
+            className={tab.id === selectedTab ? "active" : ""}
+          >
             {tab.title}
           </button>
         ))}
